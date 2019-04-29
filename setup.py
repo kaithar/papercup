7d5872d8893933a05144794c00e8b493b778202b
Kaithar <noreply.committer@the-cell.co.uk>
2019-04-29 09:01:44 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="papercup",
-    version="0.0.3",
+    version="0.0.4",
     packages=find_packages(),
     entry_points={
         'console_scripts': [
